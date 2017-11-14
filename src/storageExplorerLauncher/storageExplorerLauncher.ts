87f1e7628d5496914dde45c1f3bb4abb805312dc
Jake <jradz@me.com>
2017-11-13 16:40:43 -0800

    Change Launcher to just always pass url via commandline and let electron single instance handle the url.

--- src/storageExplorerLauncher/storageExplorerLauncher.ts
+++ src/storageExplorerLauncher/storageExplorerLauncher.ts
@@ -3,38 +3,64 @@
  *  Licensed under the MIT License. See License.txt in the project root for license information.
  *--------------------------------------------------------------------------------------------*/
 import { spawn, ChildProcess } from "child_process";
+import * as os from "os";
+import * as regedit from "regedit";
 
 export class StorageExplorerLauncher {
+    private static _regKey = "HKCR\\storageexplorer\\shell\\open\\command";
     private static _childProcess: ChildProcess;
-    public static openResource(resourceId: string, subscriptionid: string, resourceType: string, resourceName: string) {
-        StorageExplorerLauncher.ensureStorageExplorerIsOpen();
-
-        spawn(
-            "open",
-            [
-                "storageexplorer://v=1"
-                + "&accountid="
-                + encodeURIComponent(resourceId)
-                + "&subscriptionid="
-                + encodeURIComponent(subscriptionid)
-                + "&resourcetype="
-                + resourceType
-                + "&resourcename="
-                + resourceName
-            ]
-        );
+    public static async openResource(resourceId: string, subscriptionid: string, resourceType: string, resourceName: string) {
+        await StorageExplorerLauncher.launchStorageExplorer([
+            "storageexplorer://v=1"
+            + "&accountid="
+            + encodeURIComponent(resourceId)
+            + "&subscriptionid="
+            + encodeURIComponent(subscriptionid)
+            + "&resourcetype="
+            + resourceType
+            + "&resourcename="
+            + resourceName
+        ]);
     }
 
-    private static ensureStorageExplorerIsOpen() {
-        if(!StorageExplorerLauncher._childProcess) {
+    private static async getStorageExplorerExecutable(): Promise<string> {
+        if (os.platform() === "win32") {
+            return StorageExplorerLauncher.getWindowsRegistryValue(StorageExplorerLauncher._regKey).then((value) => {
+                console.log(value);
+                return value;
+            });
+
+        } else if (os.platform() === "darwin") {
+            return "/Applications/Microsoft\ Azure\ Storage\ Explorer.app/Contents/MacOS/Microsoft\ Azure\ Storage\ Explorer";
+        } else {
+            return "";
+        }
+    }
+
+    private static getWindowsRegistryValue(key: string): Promise<string> {
+        return new Promise((resolve, reject) => {
+            regedit.list([key])
+            .on('data', (entry) => {
+                var value = <string>entry.data.values[""].value.split("\"")[1];                
+                resolve(value);
+            })
+            .on('error', (err) => {
+                reject(err);
+            });
+        });
+    }
+
+    private static async launchStorageExplorer(args: string[] = []) {
+            var storageExplorerExecutable = await StorageExplorerLauncher.getStorageExplorerExecutable();
+
             var spawn_env = JSON.parse(JSON.stringify(process.env));
             // remove those env vars
             delete spawn_env.ATOM_SHELL_INTERNAL_RUN_AS_NODE;
             delete spawn_env.ELECTRON_RUN_AS_NODE;
 
             StorageExplorerLauncher._childProcess = spawn(
-                "/Applications/Microsoft\ Azure\ Storage\ Explorer.app/Contents/MacOS/Microsoft\ Azure\ Storage\ Explorer",
-                [],
+                storageExplorerExecutable,
+                args,
                 {
                     env: spawn_env
                 }
@@ -52,7 +78,6 @@ export class StorageExplorerLauncher {
                 console.log('child process exited with ' +
                 `code ${code} and signal ${signal}`);
             });
-        }
     }
 
 }
