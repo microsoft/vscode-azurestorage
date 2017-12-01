7695e3a214d2350ed1c15309aa5a12dd4efae788
Jake <Jradz@me.com>
2017-12-01 11:38:19 -0800

    Adding in download/browse logic when Storage Explorer isn't in default location.

--- src/storageExplorerLauncher/macOSStorageExplorerLauncher.ts
+++ src/storageExplorerLauncher/macOSStorageExplorerLauncher.ts
@@ -4,12 +4,38 @@
  *--------------------------------------------------------------------------------------------*/
 import { spawn, ChildProcess } from "child_process";
 import { IStorageExplorerLauncher } from "./IStorageExplorerLauncher";
+import * as vscode from 'vscode';
+import * as fs from "fs";
+
+export class UserCancelledError extends Error { }
 
 export class MacOSStorageExplorerLauncher implements IStorageExplorerLauncher {
     private _childProcess: ChildProcess;
+    private static defaultAppLocation = "/Applications/Microsoft\ Azure\ Storage\ Explorer.app";
+    private static subExecutableLocation = "/Contents/MacOS/Microsoft\ Azure\ Storage\ Explorer";
+    public static downloadPageUrl = "https://go.microsoft.com/fwlink/?LinkId=723579";
 
-    private static async getStorageExplorerExecutable(): Promise<string> {
-        return "/Applications/Microsoft\ Azure\ Storage\ Explorer.app/Contents/MacOS/Microsoft\ Azure\ Storage\ Explorer";
+    public static async getStorageExplorerExecutable(): Promise<string> {
+        var appLocation = MacOSStorageExplorerLauncher.defaultAppLocation;
+        
+        if(!(await MacOSStorageExplorerLauncher.fileExists(appLocation))) {
+            var selected: "Browse" | "Download" = <"Browse" | "Download"> await vscode.window.showWarningMessage("Could not find Storage Explorer. How would you like to resolve?", "Browse", "Download");
+            if(selected === "Browse") {
+                var selectedLocation = await MacOSStorageExplorerLauncher.showOpenDialog();
+                if(await MacOSStorageExplorerLauncher.fileExists(selectedLocation + MacOSStorageExplorerLauncher.subExecutableLocation)) {
+                    appLocation = selectedLocation;
+                    // TODO set user config value with selected location.
+                } else {
+                    // TODO prompt user that they didn't choose a storage exporer.
+                }
+            } else if(selected === "Download") {
+                appLocation = appLocation;
+            } else {
+                throw new UserCancelledError();
+            }
+        }
+
+        return appLocation +  MacOSStorageExplorerLauncher.subExecutableLocation;
     }
 
     public async openResource(resourceId: string, subscriptionid: string, resourceType: string, resourceName: string) {
@@ -73,4 +99,29 @@ export class MacOSStorageExplorerLauncher implements IStorageExplorerLauncher {
         });
     }
 
+    private static async fileExists(path: string): Promise<boolean> {
+        return await new Promise<boolean>((resolve, _reject) => {
+            fs.exists(path, (exists: boolean) => {
+                resolve(exists);
+            });
+        });
+    }
+
+    private static async showOpenDialog(): Promise<string> {
+        const defaultUri: vscode.Uri | undefined = vscode.workspace.rootPath ? vscode.Uri.file(vscode.workspace.rootPath) : undefined;
+        const options: vscode.OpenDialogOptions = {
+            defaultUri: defaultUri,
+            canSelectFiles: true,
+            canSelectFolders: false,
+            canSelectMany: false,
+            openLabel: 'select'
+        };
+        const result: vscode.Uri[] | undefined = await vscode.window.showOpenDialog(options);
+
+        if (!result || result.length === 0) {
+            throw new UserCancelledError();
+        } else {
+            return result[0].fsPath;
+        }
+    }
 }
