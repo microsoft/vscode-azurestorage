bd5282fd5c262db5beac4a4529c98a820ad73fd6
Stephen Weatherford <Stephen.Weatherford@microsoft.com>
2018-02-02 11:46:04 -0800

    Confusing don't show message, fixes #51

--- src/azureServiceExplorer/messageItems/dialogOptions.ts
+++ src/azureServiceExplorer/messageItems/dialogOptions.ts
@@ -7,6 +7,6 @@ import * as vscode from "vscode";
 
 export default class DialogOptions {
     static readonly OK: vscode.MessageItem = { title: "OK" };
-    static readonly DontShowAgain: vscode.MessageItem = { title: "Don't Show Again" };
+    static readonly UploadDontShowAgain: vscode.MessageItem = { title: "Always Upload" };
     static readonly Cancel: vscode.MessageItem = { title: "Cancel", isCloseAffordance: true };
 }
