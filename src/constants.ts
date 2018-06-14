7a5f0016e32c67fc8640efd4a28cf8d7d2240aed
Stephen Weatherford <Stephen.Weatherford@microsoft.com>
2018-06-13 17:33:38 -0700

    Support deploying to static websites

---
+++ src/constants.ts
@@ -0,0 +1,12 @@
+/*---------------------------------------------------------------------------------------------
+ *  Copyright (c) Microsoft Corporation. All rights reserved.
+ *  Licensed under the MIT License. See License.txt in the project root for license information.
+ *--------------------------------------------------------------------------------------------*/
+
+export const extensionPrefix: string = 'appService';
+
+export const staticWebsiteContainerName = '$web';
+
+export enum configurationSettingsKeys {
+    deployPath = 'deployPath'
+}
