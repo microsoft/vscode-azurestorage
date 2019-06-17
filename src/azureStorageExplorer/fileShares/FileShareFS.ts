7ee745183f89575720bb4763f1e75174ac565595
Monal Garg <monalgarg@Monals-MBP.guest.corp.microsoft.com>
2019-06-17 09:35:41 -0700

    fixing typo and better error handling

--- src/azureStorageExplorer/fileShares/FileShareFS.ts
+++ src/azureStorageExplorer/fileShares/FileShareFS.ts
@@ -94,9 +94,11 @@ export class FileShareFS implements vscode.FileSystemProvider {
 
     // tslint:disable-next-line: no-reserved-keywords
     async delete(uri: vscode.Uri, options: { recursive: boolean; }): Promise<void> {
-        return <void>await callWithTelemetryAndErrorHandling('fs.delete', async (context) => {
+        await callWithTelemetryAndErrorHandling('fs.delete', async (context) => {
+            context.errorHandling.rethrow = true;
+
             if (!options.recursive) {
-                throw new Error("Azure storage does not support nonrecusive deletion of folders.");
+                throw new Error("Azure storage does not support nonrecursive deletion of folders.");
             }
 
             let fileFound: EntryTreeItem | undefined = await this.lookup(uri, false);
