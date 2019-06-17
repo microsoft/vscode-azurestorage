a094939f34fd523cd044ce1db7ca45be3c0c292d
Monal Garg <monalgarg@Monals-MBP.guest.corp.microsoft.com>
2019-06-17 11:31:55 -0700

    create blobcontainerfs class

---
+++ src/azureStorageExplorer/blobContainers/blobContainerFS.ts
@@ -0,0 +1,42 @@
+
+/*---------------------------------------------------------------------------------------------
+ *  Copyright (c) Microsoft Corporation. All rights reserved.
+ *  Licensed under the MIT License. See License.txt in the project root for license information.
+ *--------------------------------------------------------------------------------------------*/
+
+import * as vscode from 'vscode';
+
+export class BlobContainerFS implements vscode.FileSystemProvider {
+
+    // tslint:disable-next-line: typedef
+    private _emitter = new vscode.EventEmitter<vscode.FileChangeEvent[]>();
+    readonly onDidChangeFile: vscode.Event<vscode.FileChangeEvent[]> = this._emitter.event;
+
+    watch(_uri: vscode.Uri, _options: { recursive: boolean; excludes: string[]; }): vscode.Disposable {
+        throw new Error("Method not implemented.");
+    }
+
+    stat(_uri: vscode.Uri): vscode.FileStat | Thenable<vscode.FileStat> {
+        throw new Error("Method not implemented.");
+    }
+    readDirectory(_uri: vscode.Uri): [string, vscode.FileType][] | Thenable<[string, vscode.FileType][]> {
+        throw new Error("Method not implemented.");
+    }
+    createDirectory(_uri: vscode.Uri): void | Thenable<void> {
+        throw new Error("Method not implemented.");
+    }
+    readFile(_uri: vscode.Uri): Uint8Array | Thenable<Uint8Array> {
+        throw new Error("Method not implemented.");
+    }
+    writeFile(_uri: vscode.Uri, _content: Uint8Array, _options: { create: boolean; overwrite: boolean; }): void | Thenable<void> {
+        throw new Error("Method not implemented.");
+    }
+    // tslint:disable-next-line: no-reserved-keywords
+    delete(_uri: vscode.Uri, _options: { recursive: boolean; }): void | Thenable<void> {
+        throw new Error("Method not implemented.");
+    }
+    rename(_oldUri: vscode.Uri, _newUri: vscode.Uri, _options: { overwrite: boolean; }): void | Thenable<void> {
+        throw new Error("Method not implemented.");
+    }
+
+}
