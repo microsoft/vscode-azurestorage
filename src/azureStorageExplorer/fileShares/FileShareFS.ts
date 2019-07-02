4ef9a29784b85f72264adaa690afda4be6ac2ba1
Monal Garg <monalgarg@gmail.com>
2019-07-02 15:28:16 -0700

    cleaning up createDirectory

--- src/azureStorageExplorer/fileShares/FileShareFS.ts
+++ src/azureStorageExplorer/fileShares/FileShareFS.ts
@@ -94,36 +94,34 @@ export class FileShareFS implements vscode.FileSystemProvider {
     }
 
     async createDirectory(uri: vscode.Uri): Promise<void> {
-        let parsedUri = this.parseUri(uri);
+        return await callWithTelemetryAndErrorHandling('fs.lookup', async (context) => {
+            context.errorHandling.rethrow = true;
 
-        const foundRoot = this._rootMap.get(parsedUri.fileShareName);
-        const root = !!foundRoot ? foundRoot : await this.findRoot(uri);
+            let parsedUri = this.parseUri(uri);
+            const foundRoot = this._rootMap.get(parsedUri.fileShareName);
+            const root = !!foundRoot ? foundRoot : await this.findRoot(uri);
 
-        if (root instanceof FileShareGroupTreeItem) {
-            throw new Error('Cannot create a File Share directory.');
-        }
-        if (!root) {
-            throw new Error('Cannot make a directory that is more than one level down.');
-        }
+            if (root instanceof FileShareGroupTreeItem) {
+                throw new Error('Cannot create a File Share directory.');
+            }
+            if (!root) {
+                throw new Error('Cannot make a directory within a File Share that does not exist.');
+            }
 
-        let dirName = parsedUri.baseName;
-        let parentPath = parsedUri.parentPath;
+            let response: string | undefined | null = validateDirectoryName(parsedUri.baseName);
+            if (response) {
+                throw new Error(response);
+            }
 
-        let response: string | undefined | null = validateDirectoryName(dirName);
-        if (response) {
-            throw new Error(response); // how to show to user???
-        }
+            await vscode.window.withProgress({ location: vscode.ProgressLocation.Window }, async (progress) => {
+                progress.report({ message: `Azure Storage: Creating directory '${path.posix.join(parsedUri.parentPath, parsedUri.baseName)}'` });
+                let dir = await createDirectory(root.share, root.root, parsedUri.parentPath, parsedUri.baseName);
 
-        await vscode.window.withProgress({ location: vscode.ProgressLocation.Window }, async (progress) => {
-            progress.report({ message: `Azure Storage: Creating directory '${path.posix.join(parentPath, dirName)}'` });
-            let dir = await createDirectory(root.share, root.root, parentPath, dirName);
-
-            // DirectoryResult.name contains the parent path in this call, but doesn't in other places such as listing directories.
-            // Remove it here to be consistent.
-            dir.name = dirName;
+                // DirectoryResult.name contains the parent path in this call, but doesn't in other places such as listing directories.
+                // Remove it here to be consistent.
+                dir.name = parsedUri.baseName;
+            });
         });
-
-        // have a reset here
     }
 
     async readFile(uri: vscode.Uri): Promise<Uint8Array> {
