dbf063aef5ef979d80201b5e71de73fe07e47aeb
Monal Garg <monalgarg@gmail.com>
2019-08-09 08:16:42 -0700

    making both blob and file share uris the same

--- src/extension.ts
+++ src/extension.ts
@@ -75,14 +75,14 @@ export async function activateInternal(context: vscode.ExtensionContext, perfSta
         registerCommand('azureStorage.openFileShareInFileExplorer', async (_actionContext: IActionContext, treeItem: FileShareTreeItem) => {
             await callWithTelemetryAndErrorHandling('fs.openInFileExplorer', async () => {
                 // tslint:disable-next-line: prefer-template
-                await commands.executeCommand('vscode.openFolder', vscode.Uri.parse('azurestoragefile://' + treeItem.fullId));
+                await commands.executeCommand('vscode.openFolder', vscode.Uri.parse('azurestorage://' + treeItem.fullId));
                 await commands.executeCommand('workbench.view.explorer');
             });
         });
         registerCommand('azureStorage.openBlobContainerInFileExplorer', async (_actionContext: IActionContext, treeItem: BlobContainerTreeItem) => {
             await callWithTelemetryAndErrorHandling('blob.openInFileExplorer', async () => {
                 // tslint:disable-next-line: prefer-template
-                await commands.executeCommand('vscode.openFolder', vscode.Uri.parse('azurestorageblob://' + treeItem.fullId));
+                await commands.executeCommand('vscode.openFolder', vscode.Uri.parse('azurestorage://' + treeItem.fullId));
                 await commands.executeCommand('workbench.view.explorer');
             });
         });
