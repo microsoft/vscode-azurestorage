59fa5cf2a36dcc1654750d6c96c75ff2960d01fd
William Lorey <wilorey@microsoft.com>
2019-10-02 14:08:06 -0700

    Add MS license

--- src/utils/workspaceUtils.ts
+++ src/utils/workspaceUtils.ts
@@ -1,3 +1,8 @@
+/*---------------------------------------------------------------------------------------------
+ *  Copyright (c) Microsoft Corporation. All rights reserved.
+ *  Licensed under the MIT License. See License.md in the project root for license information.
+ *--------------------------------------------------------------------------------------------*/
+
 import * as vscode from 'vscode';
 
 export function getSingleRootWorkspace(): vscode.WorkspaceFolder | undefined {
