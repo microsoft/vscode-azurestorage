c44c8b1511788ae4f0c7a605ade711766144b8fa
William Lorey <wilorey@microsoft.com>
2019-10-24 13:57:07 -0700

    Add collapse all button

--- src/extension.ts
+++ src/extension.ts
@@ -51,7 +51,7 @@ export async function activateInternal(context: vscode.ExtensionContext, perfSta
         const azureAccountTreeItem = new AzureAccountTreeItem();
         context.subscriptions.push(azureAccountTreeItem);
         ext.tree = new AzExtTreeDataProvider(azureAccountTreeItem, 'azureStorage.loadMore');
-        ext.treeView = vscode.window.createTreeView('azureStorage', { treeDataProvider: ext.tree });
+        ext.treeView = vscode.window.createTreeView('azureStorage', { treeDataProvider: ext.tree, showCollapseAll: true });
         context.subscriptions.push(ext.treeView);
 
         registerBlobActionHandlers();
