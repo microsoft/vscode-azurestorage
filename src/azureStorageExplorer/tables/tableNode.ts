e8a8b1944f874068c5f664bcc9d3f351d0a609d0
v-wuzhai <46013274+v-wuzhai@users.noreply.github.com>
2019-12-11 20:37:34 -0800

    add delete table test case (#527)

--- src/azureStorageExplorer/tables/tableNode.ts
+++ src/azureStorageExplorer/tables/tableNode.ts
@@ -4,9 +4,10 @@
  *--------------------------------------------------------------------------------------------*/
 
 import * as path from 'path';
-import { Uri, window } from 'vscode';
+import { Uri } from 'vscode';
 import { AzureParentTreeItem, AzureTreeItem, DialogResponses, UserCancelledError } from 'vscode-azureextensionui';
 import { getResourcesPath } from '../../constants';
+import { ext } from '../../extensionVariables';
 import { IStorageRoot } from "../IStorageRoot";
 
 export class TableTreeItem extends AzureTreeItem<IStorageRoot> {
@@ -26,7 +27,7 @@ export class TableTreeItem extends AzureTreeItem<IStorageRoot> {
 
     public async deleteTreeItemImpl(): Promise<void> {
         const message: string = `Are you sure you want to delete table '${this.label}' and all its contents?`;
-        const result = await window.showWarningMessage(message, { modal: true }, DialogResponses.deleteResponse, DialogResponses.cancel);
+        const result = await ext.ui.showWarningMessage(message, { modal: true }, DialogResponses.deleteResponse, DialogResponses.cancel);
         if (result === DialogResponses.deleteResponse) {
             const tableService = this.root.createTableService();
             await new Promise((resolve, reject) => {
