64ecbca0f2cdee6a93e0920d7b72aaba3fc13d5f
Will Lorey <wilorey@microsoft.com>
2020-08-03 16:35:43 -0700

    Rename 'Filename' to 'File name' in prompts (#731)

--- src/utils/validateNames.ts
+++ src/utils/validateNames.ts
@@ -44,15 +44,15 @@ export function validateFileName(name: string): string | undefined {
     const validLength = { min: 1, max: 255 };
 
     if (!name) {
-        return "Filename cannot be empty";
+        return "File name cannot be empty";
     }
 
     if (name.length < validLength.min || name.length > validLength.max) {
-        return `Filename must contain between ${validLength.min} and ${validLength.max} characters`;
+        return `File name must contain between ${validLength.min} and ${validLength.max} characters`;
     }
 
     if (invalidFileChars.some(ch => name.indexOf(ch) >= 0)) {
-        return `Filename cannot contain the following characters: ${invalidFileCharsString}'`;
+        return `File name cannot contain the following characters: ${invalidFileCharsString}'`;
     }
 
     return undefined;
