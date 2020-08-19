7c833d11dd8b7522cbde39455367a69f6d0f8916
William Lorey <wilorey@microsoft.com>
2020-08-19 15:51:09 -0700

    Only allow exec permissions for user group

--- resources/azCopy/install.sh
+++ resources/azCopy/install.sh
@@ -4,5 +4,5 @@ npm install --force --prefix . @azure-tools/azcopy-darwin \
     @azure-tools/azcopy-linux \
     @azure-tools/azcopy-win32 \
     @azure-tools/azcopy-win64
-chmod +x node_modules/@azure-tools/azcopy-darwin/dist/bin/azcopy_darwin_amd64 \
+chmod u+x node_modules/@azure-tools/azcopy-darwin/dist/bin/azcopy_darwin_amd64 \
     node_modules/@azure-tools/azcopy-linux/dist/bin/azcopy_linux_amd64
