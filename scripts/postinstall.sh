fb238286c8e72b9772fc7658e3fb88d6d6c386d4
William Lorey <wilorey@microsoft.com>
2020-08-25 15:14:41 -0700

    Remove 'npm i' from postinstall script

--- scripts/postinstall.sh
+++
@@ -1,6 +0,0 @@
-npm i --force @azure-tools/azcopy-darwin \
-    @azure-tools/azcopy-linux \
-    @azure-tools/azcopy-win32 \
-    @azure-tools/azcopy-win64
-chmod u+x node_modules/@azure-tools/azcopy-darwin/dist/bin/azcopy_darwin_amd64 \
-    node_modules/@azure-tools/azcopy-linux/dist/bin/azcopy_linux_amd64
