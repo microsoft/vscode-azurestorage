75a3ae72ae1c8ffc0b7fe4f4e19cc75d6c3e59ae
William Lorey <wilorey@microsoft.com>
2020-08-25 10:58:20 -0700

    Add install script to publish-vsix.yml
    Revert .gitignore
    Remove getAzCopyExePath

--- src/commands/azCopy/azCopyTransfer.ts
+++ src/commands/azCopy/azCopyTransfer.ts
@@ -4,9 +4,6 @@
  *--------------------------------------------------------------------------------------------*/
 
 import { AzCopyClient, AzCopyLocation, FromToOption, IAzCopyClient, ICopyOptions, ILocalLocation, IRemoteSasLocation, TransferStatus } from "@azure-tools/azcopy-node";
-import { pathExists } from 'fs-extra';
-import * as os from "os";
-import { join } from "path";
 import { callWithTelemetryAndErrorHandling, IActionContext } from "vscode-azureextensionui";
 import { ext } from '../../extensionVariables';
 import { TransferProgress } from "../../TransferProgress";
@@ -31,8 +28,7 @@ async function azCopyTransfer(
     transferProgress: TransferProgress,
     fromTo: FromToOption,
 ): Promise<void> {
-    const exe: string = await getAzCopyExePath(os.platform());
-    const copyClient: AzCopyClient = new AzCopyClient({ exe });
+    const copyClient: AzCopyClient = new AzCopyClient();
     const copyOptions: ICopyOptions = { fromTo, overwriteExisting: "true", recursive: true, followSymLinks: true };
     let jobId: string = await startAndWaitForCopy(copyClient, src, dst, copyOptions, transferProgress);
     let finalTransferStatus = (await copyClient.getJobInfo(jobId)).latestStatus;
@@ -80,39 +76,4 @@ async function startAndWaitForCopy(
     }
 
     return jobId;
-}
-
-async function getAzCopyExePath(platform: NodeJS.Platform): Promise<string> {
-    let operatingSystem: 'win32' | 'win64' | 'darwin' | 'linux';
-    let bitness: '64' | '86';
-    const isWindows: boolean = platform === 'win32';
-    if (isWindows) {
-        if (process.arch.toLowerCase() === 'x64' || process.env.hasOwnProperty('PROCESSOR_ARCHITEW6432')) {
-            operatingSystem = 'win64';
-            bitness = '64';
-        } else {
-            operatingSystem = 'win32';
-            bitness = '86';
-        }
-    } else if (platform === 'darwin' || platform === 'linux') {
-        operatingSystem = platform;
-        bitness = '64';
-    } else {
-        throw new RangeError(localize('unexpectedPlatform', 'Unexpected platform "{0}"', platform));
-    }
-
-    const webpackParentPath: string = join(ext.context.asAbsolutePath('dist'), 'node_modules');
-    const debugParentPath: string = ext.context.asAbsolutePath('node_modules');
-    const parentPathsToTry: string[] = [webpackParentPath, debugParentPath];
-    for (const parentPath of parentPathsToTry) {
-        let exePath: string = join(parentPath, '@azure-tools', `azcopy-${operatingSystem}`, 'dist', 'bin', `azcopy_${isWindows ? 'windows' : operatingSystem}_amd${bitness}`);
-        if (isWindows) {
-            exePath += '.exe';
-        }
-        if (await pathExists(exePath)) {
-            return exePath;
-        }
-    }
-
-    throw new Error(localize('azCopyExeNotFound', 'AzCopy executable not found'));
 }
