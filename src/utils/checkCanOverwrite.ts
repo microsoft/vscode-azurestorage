60602423db618190ac228d8b7e9b754f5e34cb2b
William Lorey <wilorey@microsoft.com>
2020-10-20 13:34:31 -0700

    Reserve those rights

--- src/utils/checkCanOverwrite.ts
+++ src/utils/checkCanOverwrite.ts
@@ -1,3 +1,8 @@
+/*---------------------------------------------------------------------------------------------
+ *  Copyright (c) Microsoft Corporation. All rights reserved.
+ *  Licensed under the MIT License. See License.md in the project root for license information.
+ *--------------------------------------------------------------------------------------------*/
+
 import { DialogResponses } from "vscode-azureextensionui";
 import { ext } from "../extensionVariables";
 import { localize } from "./localize";
