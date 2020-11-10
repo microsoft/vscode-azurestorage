4f15c16b60cdc9aff790343c4f69654a5e50d440
Will Lorey <wilorey@microsoft.com>
2020-11-10 13:12:52 -0800

    Add missing AzCopy error message (#881)

--- src/commands/azCopy/azCopyTransfer.ts
+++ src/commands/azCopy/azCopyTransfer.ts
@@ -43,7 +43,7 @@ async function handleJob(context: IActionContext, jobInfo: IJobInfo, transferLab
     context.telemetry.properties.jobStatus = finalTransferStatus?.JobStatus;
     if (!finalTransferStatus || finalTransferStatus.JobStatus !== 'Completed') {
         // tslint:disable-next-line: strict-boolean-expressions
-        let message: string = localize('azCopyTransfer', 'AzCopy Transfer: "{0}".', finalTransferStatus?.JobStatus || 'Failed');
+        let message: string = jobInfo.errorMessage || localize('azCopyTransfer', 'AzCopy Transfer: "{0}". ', finalTransferStatus?.JobStatus || 'Failed');
         if (finalTransferStatus?.FailedTransfers?.length || finalTransferStatus?.SkippedTransfers?.length) {
             message += localize('checkOutputWindow', ' Check the [output window](command:{0}) for a list of incomplete transfers.', `${ext.prefix}.showOutputChannel`);
 
