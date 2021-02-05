3d922d0fb49e6a45c5eca6bb71250d8aadc92f36
William Lorey <wilorey@microsoft.com>
2021-02-04 17:47:11 -0800

    Fix lint errors in test/

--- test/index.ts
+++ test/index.ts
@@ -49,7 +49,7 @@ function addEnvVarsToMochaOptions(options: Mocha.MochaOptions): void {
             if (typeof value === 'string' && !isNaN(parseInt(value))) {
                 value = parseInt(value);
             }
-            // tslint:disable-next-line: no-any
+            // eslint-disable-next-line @typescript-eslint/no-unsafe-member-access, @typescript-eslint/no-explicit-any
             (<any>options)[option] = value;
         }
     }
