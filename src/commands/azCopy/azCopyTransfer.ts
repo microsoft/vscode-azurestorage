db5f606f64ec37aaee0eef3919de4ef6b3e1ef77
Will Lorey <wilorey@microsoft.com>
2021-05-26 17:30:00 -0700

    Update check for showing 'known issues' link (AzCopy) (#976)
    * Update check for showing 'known issues' link (AzCopy)
    
    * Show wiki link more often

--- src/commands/azCopy/azCopyTransfer.ts
+++ src/commands/azCopy/azCopyTransfer.ts
@@ -62,9 +62,8 @@ function handleJob(context: IActionContext, jobInfo: IJobInfo, transferLabel: st
             // Add an additional error log since we don't have any more info about the failure
             ext.outputChannel.appendLog(localize('couldNotTransfer', 'Could not transfer "{0}"', transferLabel));
 
-            const isDefaultFailMessage: boolean = !jobInfo.errorMessage && finalTransferStatus?.JobStatus === 'Failed';
-            if (isDefaultFailMessage && process.platform === 'linux') {
-                message += localize('viewHelp', 'View help with [known issues](https://aka.ms/AAb0i6o).');
+            if (finalTransferStatus?.JobStatus === 'Failed' && process.platform === 'linux') {
+                message += localize('viewHelp', ' View help with [known issues](https://aka.ms/AAb0i6o).');
             }
         }
 
