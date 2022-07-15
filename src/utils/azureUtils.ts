9d825fa9548b6f2b8a0538ac007d86db1ee8f8ef
Alex Weininger <alex.weininger@live.com>
2022-07-15 12:56:30 -0700

    Use shared azure id parsing util (#1094)

--- src/utils/azureUtils.ts
+++ src/utils/azureUtils.ts
@@ -1,18 +1,7 @@
-import { localize } from "./localize";
-
-function parseResourceId(id: string): RegExpMatchArray {
-    const matches: RegExpMatchArray | null = id.match(/\/subscriptions\/(.*)\/resourceGroups\/(.*)\/providers\/(.*)\/(.*)/);
-
-    if (matches === null || matches.length < 3) {
-        throw new Error(localize('InvalidResourceId', 'Invalid Azure Resource Id'));
-    }
-
-    return matches;
-}
-
-export function getResourceGroupFromId(id: string): string {
-    return parseResourceId(id)[2];
-}
+/*---------------------------------------------------------------------------------------------
+*  Copyright (c) Microsoft Corporation. All rights reserved.
+*  Licensed under the MIT License. See License.txt in the project root for license information.
+*--------------------------------------------------------------------------------------------*/
 
 export function getAppResourceIdFromId(id: string): string | undefined {
     const matches: RegExpMatchArray | null = id.match(/\/subscriptions\/.*\/resourceGroups\/.*\/providers\/Microsoft.Storage\/storageAccounts\/[^\/]*/);
