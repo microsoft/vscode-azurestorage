524070a680bd987da0cb4b6e0649ce25a3fd845c
MicroFish91 <mwfisher91@gmail.com>
2022-08-11 11:24:05 -0700

    deleteBlob

--- src/commands/blob/blobActionHandlers.ts
+++ src/commands/blob/blobActionHandlers.ts
@@ -4,14 +4,25 @@
  *--------------------------------------------------------------------------------------------*/
 
 import { IActionContext, registerCommand } from "@microsoft/vscode-azext-utils";
+import { ext } from "../../extensionVariables";
 import { BlobDirectoryTreeItem } from "../../tree/blob/BlobDirectoryTreeItem";
 import { BlobTreeItem } from "../../tree/blob/BlobTreeItem";
 
 export function registerBlobActionHandlers(): void {
     registerCommand("azureStorage.deleteBlob", deleteBlob);
-    registerCommand("azureStorage.deleteBlobDirectory", async (context: IActionContext, treeItem: BlobDirectoryTreeItem) => await treeItem.deleteTreeItem(context));
 }
 
-export async function deleteBlob(context: IActionContext, treeItem: BlobTreeItem): Promise<void> {
-    await treeItem.deleteTreeItem(context);
+export async function deleteBlob(context: IActionContext, treeItem: BlobTreeItem | BlobDirectoryTreeItem): Promise<void> {
+    const { selection } = ext.rgApi.appResourceTreeView;
+
+    // Covers both single selection and an edge case where the node you delete from isn't part of the selection
+    if (!selection.some(s => s === treeItem)) {
+        await ext.rgApi.appResourceTreeView.reveal(treeItem);
+        await treeItem.deleteTreeItem(context);
+        return;
+    }
+
+    for (const node of selection) {
+        await node.deleteTreeItem(context);
+    }
 }
