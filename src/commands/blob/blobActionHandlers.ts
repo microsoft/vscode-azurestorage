e9fc04a4a4c4f1960bc20910ea82e25a03081ee8
MicroFish91 <mwfisher91@gmail.com>
2022-08-15 13:28:09 -0700

    consolidate file and directory based commands

--- src/commands/blob/blobActionHandlers.ts
+++ src/commands/blob/blobActionHandlers.ts
@@ -3,9 +3,6 @@
  *  Licensed under the MIT License. See License.md in the project root for license information.
  *--------------------------------------------------------------------------------------------*/
 
-import { registerCommand } from "@microsoft/vscode-azext-utils";
-import { deleteFilesAndDirectories } from "../deleteFilesAndDirectories";
-
 export function registerBlobActionHandlers(): void {
-    registerCommand("azureStorage.deleteBlob", deleteFilesAndDirectories);
+    // Placeholder
 }
