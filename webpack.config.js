b98f608a9a0131310d9085a7f1716f77c22d250b
Matthew Rayermann <marayerm@microsoft.com>
2023-09-11 13:48:45 -0700

    Remove unneeded externals, add empty package.nls.json

--- webpack.config.js
+++ webpack.config.js
@@ -53,13 +53,6 @@ const webConfig = dev.getDefaultWebpackConfig({
         "@azure-tools/azcopy-darwin": "@azure-tools/azcopy-darwin",
         // Since AzCopy exes are not available, exclude the root azcopy-node wrapper package
         "@azure-tools/azcopy-node": "@azure-tools/azcopy-node",
-        // Fix "Module not found" errors in ./node_modules/websocket/lib/{BufferUtil,Validation}.js
-        // These files are not in node_modules and so will fail normally at runtime and instead use fallbacks.
-        // Make them as external so webpack doesn't try to process them, and they'll simply fail at runtime as before.
-        '../build/Release/validation': 'commonjs ../build/Release/validation',
-        '../build/default/validation': 'commonjs ../build/default/validation',
-        '../build/Release/bufferutil': 'commonjs ../build/Release/bufferutil',
-        '../build/default/bufferutil': 'commonjs ../build/default/bufferutil',
     },
     target: 'webworker',
     plugins: [
