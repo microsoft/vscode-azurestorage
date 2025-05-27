6437548d462de36bd46a1964a09bd984cccfe8be
Nathan Turinski <naturins@microsoft.com>
2025-05-27 11:02:24 -0700

    Delete file share directory execute step

--- src/utils/directoryUtils.ts
+++ src/utils/directoryUtils.ts
@@ -5,15 +5,17 @@
 
 import type { DirectoryItem, DirectoryListFilesAndDirectoriesSegmentResponse, FileItem, ShareDirectoryClient } from "@azure/storage-file-share";
 
-import { AzExtTreeItem, ICreateChildImplContext } from "@microsoft/vscode-azext-utils";
+import { AzExtTreeItem, AzureWizard, AzureWizardExecuteStepWithActivityOutput, callWithTelemetryAndErrorHandling, IActionContext, ICreateChildImplContext } from "@microsoft/vscode-azext-utils";
 import * as path from "path";
-import { ProgressLocation, window } from "vscode";
+import { Progress, ProgressLocation, window } from "vscode";
 import { maxPageSize } from "../constants";
 import { ext } from "../extensionVariables";
 import { IStorageRoot } from "../tree/IStorageRoot";
 import { DirectoryTreeItem } from "../tree/fileShare/DirectoryTreeItem";
 import { FileShareTreeItem, IFileShareCreateChildContext } from "../tree/fileShare/FileShareTreeItem";
+import { createActivityContext } from "./activityUtils";
 import { createDirectoryClient, deleteFile, getFileOrDirectoryName } from "./fileUtils";
+import { localize } from "./localize";
 
 // Supports both file share and directory parents
 export async function askAndCreateChildDirectory(parent: FileShareTreeItem | DirectoryTreeItem, parentPath: string, shareName: string, context: ICreateChildImplContext & IFileShareCreateChildContext): Promise<DirectoryTreeItem> {
@@ -45,35 +47,74 @@ export async function deleteDirectoryAndContents(directory: string, shareName: s
     const parallelOperations = 5;
 
     let currentToken: string | undefined;
-    // eslint-disable-next-line no-constant-condition
-    while (true) {
-        const { files, directories, continuationToken }: { files: FileItem[], directories: DirectoryItem[], continuationToken: string } = await listFilesInDirectory(directory, shareName, root, currentToken);
-        let promises: Promise<void>[] = [];
-        for (const file of files) {
-            const promise = deleteFile(directory, file.name, shareName, root);
-            promises.push(promise);
-            ext.outputChannel.appendLog(`Deleted file "${directory}/${file.name}"`);
+    await callWithTelemetryAndErrorHandling('deleteDirectoryAndContents', async (context) => {
 
-            if (promises.length >= parallelOperations) {
-                await Promise.all(promises);
-                promises = [];
+
+        // eslint-disable-next-line no-constant-condition
+        while (true) {
+            const { files, directories, continuationToken }: { files: FileItem[], directories: DirectoryItem[], continuationToken: string } = await listFilesInDirectory(directory, shareName, root, currentToken);
+            let promises: Promise<void>[] = [];
+            for (const file of files) {
+                const promise = deleteFile(directory, file.name, shareName, root);
+                promises.push(promise);
+                ext.outputChannel.appendLog(`Deleted file "${directory}/${file.name}"`);
+
+                if (promises.length >= parallelOperations) {
+                    await Promise.all(promises);
+                    promises = [];
+                }
+            }
+            await Promise.all(promises);
+
+            for (const dir of directories) {
+                await deleteDirectoryAndContents(path.posix.join(directory, dir.name), shareName, root);
+            }
+
+            currentToken = continuationToken;
+            if (!currentToken) {
+                break;
             }
         }
-        await Promise.all(promises);
+        const executeSteps = [new DeleteDirectoryExecuteStep(root, shareName, directory)];
+        const deletingDirectory: string = localize('deleteDirectory', 'Delete directory "{0}"', directory);
+        const wizardContext = Object.assign(context, {
+            ...(await createActivityContext()),
+            activityTitle: deletingDirectory
+        });
 
-        for (const dir of directories) {
-            await deleteDirectoryAndContents(path.posix.join(directory, dir.name), shareName, root);
-        }
+        const wizard = new AzureWizard(wizardContext, {
+            title: deletingDirectory,
+            executeSteps
+        });
 
-        currentToken = continuationToken;
-        if (!currentToken) {
-            break;
-        }
+        await wizard.execute();
+    });
+}
+
+class DeleteDirectoryExecuteStep extends AzureWizardExecuteStepWithActivityOutput<IActionContext> {
+    stepName: string = 'DeleteDirectoryExecuteStep';
+    protected getTreeItemLabel(_context: IActionContext): string {
+        return localize('deleteDirectory', 'Delete directory "{0}".', this.directory);
+    }
+    protected getOutputLogSuccess(_context: IActionContext): string {
+        return localize('deletedDirectory', 'Deleted directory "{0}".', this.directory);
+    }
+    protected getOutputLogFail(_context: IActionContext): string {
+        return localize('deletedDirectoryFailed', 'Failed to delete directory "{0}".', this.directory);
+    }
+    public priority: number;
+
+    public constructor(readonly root: IStorageRoot, readonly shareName: string, readonly directory: string) {
+        super();
+    }
+    public async execute(_context: IActionContext, _progress: Progress<{ message?: string; increment?: number; }>): Promise<void> {
+        const directoryClient: ShareDirectoryClient = await createDirectoryClient(this.root, this.shareName, this.directory);
+        await directoryClient.delete();
     }
 
-    const directoryClient: ShareDirectoryClient = await createDirectoryClient(root, shareName, directory);
-    await directoryClient.delete();
-    ext.outputChannel.appendLog(`Deleted directory "${directory}"`);
+    public shouldExecute(_context: IActionContext): boolean {
+        return true;
+    }
 }
 
 export async function doesDirectoryExist(parent: FileShareTreeItem | DirectoryTreeItem, directoryPath: string, shareName: string): Promise<boolean> {
