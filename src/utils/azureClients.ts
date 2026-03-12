e935339e5ce03867e9dad4f5767fdb74dbbd0ada
Brandon Waterloo [MSFT] <36966225+bwateratmsft@users.noreply.github.com>
2026-03-12 09:33:32 -0400

    Fix busted azureclients

--- src/utils/azureClients.ts
+++ src/utils/azureClients.ts
@@ -3,9 +3,9 @@
  *  Licensed under the MIT License. See License.txt in the project root for license information.
  *--------------------------------------------------------------------------------------------*/
 
-import { type ResourceGraphClient } from '@azure/arm-resourcegraph';
-import { StorageManagementClient } from '@azure/arm-storage';
-import { AzExtClientContext, createAzureClient, createAzureSubscriptionClient, parseClientContext } from '@microsoft/vscode-azext-azureutils';
+import type { ResourceGraphClient } from '@azure/arm-resourcegraph';
+import type { StorageManagementClient } from '@azure/arm-storage';
+import { AzExtClientContext, AzExtSubscriptionClientType, createAzureSubscriptionClient, createStorageClient as createStorageClientImpl, parseClientContext } from '@microsoft/vscode-azext-azureutils';
 
 // Lazy-load @azure packages to improve startup performance.
 // NOTE: The client is the only import that matters, the rest of the types disappear when compiled to JavaScript
@@ -14,12 +14,11 @@ export async function createStorageClient(context: AzExtClientContext): Promise<
     if (parseClientContext(context).isCustomCloud) {
         // set API version for Azure Stack
         process.env.AZCOPY_DEFAULT_SERVICE_API_VERSION = "2019-02-02";
-        return <StorageManagementClient><unknown>createAzureClient(context, (await import('@azure/arm-storage-profile-2020-09-01-hybrid')).StorageManagementClient);
-    } else {
-        return createAzureClient(context, (await import('@azure/arm-storage')).StorageManagementClient);
     }
+
+    return createStorageClientImpl(context);
 }
 
 export async function createResourceGraphClient(context: AzExtClientContext): Promise<ResourceGraphClient> {
-    return createAzureSubscriptionClient(context, (await import('@azure/arm-resourcegraph')).ResourceGraphClient);
+    return createAzureSubscriptionClient(context, (await import('@azure/arm-resourcegraph')).ResourceGraphClient as unknown as AzExtSubscriptionClientType<ResourceGraphClient>);
 }
